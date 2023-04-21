0000f36fbcca4db07b7990d653215eb8bc355467
JenifferBarrios <jbarrios03@hotmail.com>
2023-04-21 12:52:12 -0500

    Trabajando la funcionalidad al encriptador: se creo la funcion que logra capturar el tecto ingresado por el usuario y mostrarlo en una alerta

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// Se crea una funcion que captura el valor ingresado por el usuario
+
+const boton = document.getElementById("encriptar");
+
+boton.addEventListener("click", () => {
+
+  var input = document.getElementById("texto");
+  var valor = input.value;
+
+  alert("El valor del campo es: " + valor);
+  
+  // Que es valor capturado se recorra con un for y cuando encuentre las las vocales cambiarlas por lo que se muestra a
+  contonuacion :
+  //La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+//arrojar un mensaje encriptado
+  const conversorTexto = () => {
+    if()
+    for (let i = 0; i < 5; i++) {
+        text += "The number is " + i + "<br>";
+      }
+      
+  }
+});
+ 
