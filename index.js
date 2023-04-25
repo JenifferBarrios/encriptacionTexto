2079208df8ab607748307fd7750f855b03e56fd4
JenifferBarrios <jbarrios03@hotmail.com>
2023-04-25 12:43:18 -0500

    Creando las condicionales para el cmabio de vocales

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 
 // Se crea una funcion que captura el valor ingresado por el usuario
 
-const boton = document.getElementById("encriptar");
-
-boton.addEventListener("click", () => {
+// const boton = document.getElementById("encriptar");
+const botondos = document.getElementById("encriptar")
+botondos.addEventListener("click", () => {
 
   var input = document.getElementById("texto");
   var valor = input.value;
 
-  alert("El valor del campo es: " + valor);
-  
-  // Que es valor capturado se recorra con un for y cuando encuentre las las vocales cambiarlas por lo que se muestra a
-  contonuacion :
-  //La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-//arrojar un mensaje encriptado
-  const conversorTexto = () => {
-    if()
-    for (let i = 0; i < 5; i++) {
-        text += "The number is " + i + "<br>";
-      }
-      
+  // alert("El valor del campo es: " + valor);
+
+
+  let texto = ""
+  for (let i = 0; i < valor.length; i++) {
+    // console.log("que trae la i", valor[i])
+    if (valor[i] == "a") {
+      texto += "ai"
+    }
+    else if (valor[i] == "e") {
+      texto += "enter"
+    }
+    else if (valor[i] == "i") {
+      texto += "imes"
+    }
+    else if (valor[i] == "o") {
+      texto += "ober"
+    }
+    else if (valor[i] == "u") {
+      texto += "ufat"
+    } else {
+      texto += valor[i]
+    }
+
   }
+//   console.log("cambia las vocales", texto)
+const msjEncriptado = document.querySelector("#textoMostrado")
+
+msjEncriptado
+
+
+   
 });
- 
